f60eca9894e0be83740c1659801498fc638e74af
franciscogar94 <franciscogar94@gmail.com>
2023-04-27 13:15:47 -0400

    actualizado a angular 15

--- src/ion-bottom-sheet.module.ts
+++ src/ion-bottom-sheet.module.ts
@@ -1,17 +1,16 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
-import { IonBottomSheetComponent } from './components/ion-bottom-sheet.component';
-import { IonBottomSheetProvider } from './providers/ion-bottom-sheet-provider';
-
+import { NgModule, ModuleWithProviders } from "@angular/core";
+import { IonBottomSheetComponent } from "./components/ion-bottom-sheet.component";
+import { IonBottomSheetProvider } from "./providers/ion-bottom-sheet-provider";
 
 @NgModule({
-  declarations: [ IonBottomSheetComponent ],
-  exports: [ IonBottomSheetComponent ]
+  declarations: [IonBottomSheetComponent],
+  exports: [IonBottomSheetComponent],
 })
 export class IonBottomSheetModule {
-  static forRoot(): ModuleWithProviders {
+  static forRoot(): ModuleWithProviders<IonBottomSheetModule> {
     return {
       ngModule: IonBottomSheetModule,
-      providers: [ IonBottomSheetProvider ]
+      providers: [IonBottomSheetProvider],
     };
   }
- }
+}
